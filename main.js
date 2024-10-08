706ba8cbd3f6a497313150c318708f23bc6b75d5
ivantarabanovych <vanjokcitet334@gmail.com>
2024-10-08 17:08:26 +0300

    q2

--- main.js
+++ main.js
@@ -1,61 +1,22 @@
-fetch('https://study.todor.academy/', {
-    mode: 'no-cors'
-  })
-  .then(response => {
-    console.log(response);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
-
-  fetch('https://cors-anywhere.herokuapp.com/https://study.todor.academy/')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error('Error:', error));
-
-
-
-  fetch('https://study.todor.academy/', {
-    method: 'POST',
-    headers: {
-        'Content-Type' : 'aplication/json',
-    }, 
-    body: JSON.stringify({key: 'value'})
-  })
-  .then(response => {
-    console.log('Status:', response.status);
-    console.log('Headers:', response.headers);
-    return response.json();
-  })
-  .then(data => console.log('Data:', data))
-  .catch(error => console.error('Error:', error));
-
-
-
-  fetch('https://study.todor.academy/', {
-    method: 'PUT',
-    headers: {
-        'Content-Type' : 'aplication/json',
-    }, 
-    body: JSON.stringify({key: 'newValue'})
-  })
-  .then(response => {
-    console.log('Status:', response.status);
-    console.log('Headers:', response.headers);
-    return response.json();
-  })
-  .then(data => console.log('Data:', data))
-  .catch(error => console.error('Error:', error));
-
-
-
-  fetch('https://study.todor.academy/', {
-    method: 'DELETE',
-  })
-  .then(response => {
-    console.log('Status:', response.status);
-    console.log('Headers:', response.headers);
-    return response.json();
-  })
-  .then(data => console.log('Data:', data))
-  .catch(error => console.error('Error:', error));
+//-------------------------------------------------
+// URI https://study.todor.academy/profile/progress
+// протокол https
+// домен study.todor.academy	
+// шлях /profile/progress
+// параметрів немає 
+// сторінка профілю студента з прогресом 
+//-------------------------------------------------
+// URI https://study.todor.academy/profile/settings
+// протокол https
+// домен study.todor.academy	
+// шлях /profile/settings
+// параметрів немає 
+// сторінка профілю налаштування акаунту
+//-------------------------------------------------
+//URI https://study.todor.academy/profile/settings#
+// протокол https
+// домен study.todor.academy
+// шлях /profile/progress
+// параметрів немає 
+// якір #
+// сторінка профілю налаштування при переході на темну тему
