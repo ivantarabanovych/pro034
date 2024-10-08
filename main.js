82ac4894a6f2a164bba360abb5bf8455ccf1488e
ivantarabanovych <vanjokcitet334@gmail.com>
2024-10-08 17:19:20 +0300

    q3

--- main.js
+++ main.js
@@ -1,22 +1,30 @@
-//-------------------------------------------------
-// URI https://study.todor.academy/profile/progress
-// протокол https
-// домен study.todor.academy	
-// шлях /profile/progress
-// параметрів немає 
-// сторінка профілю студента з прогресом 
-//-------------------------------------------------
-// URI https://study.todor.academy/profile/settings
-// протокол https
-// домен study.todor.academy	
-// шлях /profile/settings
-// параметрів немає 
-// сторінка профілю налаштування акаунту
-//-------------------------------------------------
-//URI https://study.todor.academy/profile/settings#
-// протокол https
-// домен study.todor.academy
-// шлях /profile/progress
-// параметрів немає 
-// якір #
-// сторінка профілю налаштування при переході на темну тему
+fetch('https://study.todor.academy/profile/settings')
+  .then(response => {
+    console.log(response.status); 
+    return response.text();
+  })
+  .then(data => console.log(data))
+  .catch(error => console.error('Error:', error));
+
+  fetch('https://study.todor.academy/nonexistent-page')
+  .then(response => {
+    console.log(response.status); 
+  })
+  .catch(error => console.error('Error:', error));
+
+  fetch ('https://study.todor.academy/api/data', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({invalid: "data"})
+  })
+  .then(response => {
+    console.log(response.status);
+  })
+.catch(error => console.error('ERROR:', error));
+
+
+// 200 OK Запит успішно оброблено, ресурс знайдено GET https://study.todor.academy/profile/settings
+// 404 not found Запитаний ресурс не знайдено на сервері GET https://study.todor.academy/nonexistent-page
+// 500 Internal Server Error Виникла помилка на сервері. POST https://study.todor.academy/api/data з некоректними даними
